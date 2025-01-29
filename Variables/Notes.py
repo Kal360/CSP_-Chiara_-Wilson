5f2282245b76f51f44315617c898bfbc457d5e17
kal360 <chiara.negron@ucas-edu.net>
2025-01-29 11:43:10 -0700

    Python Notes

--- Variables/Notes.py
+++ Variables/Notes.py
@@ -1,3 +1,13 @@
 # Chiara Ivelisse Negron Wilson, variable notes
 
+name = "Chiara"
+
+subject = input("what is your favorite subject: ")
+
+
+small_number = 4
 print("It worked !")
+name = input(" What is your name: ")
+
+print(name, " really likes",subject)
+print(subject)
