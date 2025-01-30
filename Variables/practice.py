9f242b44a12ec0009a5eea35aba18599dc50fa57
kal360 <chiara.negron@ucas-edu.net>
2025-01-30 11:50:52 -0700

    c notes

--- Variables/practice.py
+++ Variables/practice.py
@@ -1,1 +1,15 @@
 name = " Chiara"
+
+print("my name is: ")
+
+number=5
+
+print(" I love the",number )
+
+Tripledigit = 500
+
+print(Tripledigit, "Is awesome")
+
+Breakfast= " cereal"
+
+print("I ate")
---
+++ template.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+char name[] = "Chiara" ;
+int age = 15;
+int main(void){
+    printf("  Hello I am%s. I am %d years old. I like\n", name);
+    printf("%d\n",age)
+int float pi =3.14;
+   prinf=tf("%f\n",float)
+    return 0;
+} 
