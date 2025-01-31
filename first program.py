0eb8dcee439ceec8e74437f2ac5161021a085b2e
kal360 <chiara.negron@ucas-edu.net>
2025-01-31 11:48:08 -0700

    made variable and input for python project

---
+++ first program.py
@@ -0,0 +1,17 @@
+# Chiara Ivelisse Negron Wilson, first program
+
+name = "Chiara"
+
+subject = input("what is your favorite subject: ")
+
+
+small_number = 4
+
+name = input(" What is your name: ")
+
+print(name, " really likes",subject)
+print(subject)
+
+eye = " green"
+
+eye= input(" what is your eye color: ")
