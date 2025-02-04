c076004558a9765710e2765874bd42da18ec889d
Kal360 <chiara.negron@ucas-edu.net>
2025-02-04 19:17:04 +0000

    I wrote equations in python practice

--- Expressions/practice.py
+++ Expressions/practice.py
@@ -15,3 +15,9 @@ print(orderofmathops+(6+2-3))
 squarebrakets= -1*((3-4*7)/5)
 
 print(squarebrakets-2*24/6)
+
+print((3*5**2/15)-(5-2**2))
+
+print((1**4*2**2+3**3)-2**5/4)
+
+print((22/2-2*5)**2 + (4-6/6)**2)
