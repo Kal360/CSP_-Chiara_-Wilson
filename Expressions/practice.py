16118173358a6a312201c2ed3e09a1c549240458
kal360 <chiara.negron@ucas-edu.net>
2025-02-04 11:49:19 -0700

    I wrote out math in python

---
+++ Expressions/practice.py
@@ -0,0 +1,17 @@
+# Chiara Negron Wilson, Expresions Practice
+
+print(7-24/8*4+6)
+
+print(18/3-7+2*5)
+
+print(6*4/12+72/8-9)
+
+print((17-6/2)+ 4*3)
+
+orderofmathops = -2*(1*4-2/2)
+
+print(orderofmathops+(6+2-3))
+
+squarebrakets= -1*((3-4*7)/5)
+
+print(squarebrakets-2*24/6)
