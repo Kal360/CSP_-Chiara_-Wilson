b4dcbc09f427626bcbf8a96fcc20a2c681797e3f
kal360 <chiara.negron@ucas-edu.net>
2025-02-06 11:48:49 -0700

    I code a finnacial calculator on c and python

--- Expressions/financecal.py
+++ Expressions/financecal.py
@@ -1,1 +1,38 @@
-# Chiara
+# Chiara Wilson, Finacial calculator python
+
+# print statement that welcomes my user and tells them what the program does
+
+# ask user what their income is ( variable an input)
+income= float(input("What is your income?\n"))
+# ask user what their rent is ( variable an input)
+rent=float(input("What is your rent?\n"))
+#ask user what their utilites is ( variable an input)
+utilites=float(input("How much do you spend on utilites?\n"))
+#ask user what their groceries is ( variable an input)
+groceries=float(input("How do you spend on groceries?\n"))
+#ask user what their transportation is ( variable an input)
+transportation=float(input(" How much do you spend transportation"))
+# calculate savings as 10% of income (income * .1)( variable)
+savings=income*0.1
+# calculate spending as income- savings-rent-utilites-groceries-transportaion ( variable)
+spending=income-savings-rent-utilites-groceries-transportation-savings
+# calculate percent income of rent(rent/income *100) ( variable)
+rentpercent= (rent/income)*100
+# calculate percent income of utilites (utilites/income *100) ( variable)
+utilpercent=(utilites/income)*100
+# calculate percent income of groceries (groceries/income *100) ( variable)
+groceriespercent=(groceries)
+# calculate percent income of transportation(transportation/income *100) ( variable)
+
+# calculate percent income of spending(spending/income *100) ( variable)
+
+
+# Your utilites is $XX.XX which is XX% of your income. (Print)
+
+# Your groceries is $XX.XX which is XX% of your income. (Print)
+
+# Your transportation is $XX.XX which is XX% of your income. (Print)
+
+# Your savings is $XX.XX which is XX% of your income. (print)
+
+# Your spending is $XX.XX which is XX% of your income. (Print)
---
+++ Expressions/financial_calculator.c
@@ -0,0 +1,43 @@
+// Chiara,Wilson
+#include <stdio.h>
+
+//print statement that welcomes my user and tells them what the program does
+
+//ask user what their income is ( variable an input)
+
+//ask user what their rent is ( variable an input)
+
+//#ask user what their utilites is ( variable an input)
+
+//#ask user what their groceries is ( variable an input)
+
+//ask user what their transportation is ( variable an input)
+
+ //calculate savings as 10% of income (income * .1)( variable)
+
+//calculate spending as income- savings-rent-utilites-groceries-transportaion ( variable)
+
+//calculate percent income of rent(rent/income *100) ( variable)
+
+//calculate percent income of utilites (utilites/income *100) ( variable)
+
+//calculate percent income of groceries (groceries/income *100) ( variable)
+
+//calculate percent income of transportation(transportation/income *100) ( variable)
+
+//calculate percent income of spending(spending/income *100) ( variable)
+
+
+// Your utilites is $XX.XX which is XX% of your income. (Print)
+
+//Your groceries is $XX.XX which is XX% of your income. (Print)
+
+//Your transportation is $XX.XX which is XX% of your income. (Print)
+
+// Your savings is $XX.XX which is XX% of your income. (print)
+
+// Your spending is $XX.XX which is XX% of your income. (Print)
+int main(void){
+    printf("hello world");
+    return 0;
+} 
