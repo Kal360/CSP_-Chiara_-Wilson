aa8a13eab148d1bc20500feda278ef8cc50fd21d
kal360 <chiara.negron@ucas-edu.net>
2025-02-10 11:49:49 -0700

    I did notes in python

---
+++ Strings/notes.py
@@ -0,0 +1,13 @@
+# Chiara Wilson, Strings Notes Python
+
+# string is a data type that pulls any  information surronded by quotation marks. "" ''
+
+note="Chiara's class"
+
+name= (input(" What is your first name?\n").strip().capitalize())
+
+#print(f"Hello {name} welcome to my thingy!\n")
+
+print(" This is your name")
+
+sentence= "The quick brown fox jumps over the lazy dog.\n"
