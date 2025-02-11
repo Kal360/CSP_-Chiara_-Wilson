5bf9e2290a439fec798d87aef055f07fd32982f5
kal360 <chiara.negron@ucas-edu.net>
2025-02-11 11:15:33 -0700

    I coded notes in python

--- Strings/notes.py
+++ Strings/notes.py
@@ -4,10 +4,15 @@
 
 note="Chiara's class"
 
-name= (input(" What is your first name?\n").strip().capitalize())
+#name= (input(" What is your first name?\n").strip().capitalize())
 
 #print(f"Hello {name} welcome to my thingy!\n")
 
-print(" This is your name")
+#print(" This is your name "+ name)
 
-sentence= "The quick brown fox jumps over the lazy dog.\n"
+sentence= "The quick brown fox jumps over the lazy dog."
+
+#print(len(sentence))
+start= sentence.find("brown")
+length=len("brown fox")
+print(sentence[start:start+length])
