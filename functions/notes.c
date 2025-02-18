9f11452d470d6bd23bf473a0020af65176d5b09e
kal360 <chiara.negron@ucas-edu.net>
2025-02-18 11:49:25 -0700

    I coded notes in Cand python

--- functions/notes.c
+++ functions/notes.c
@@ -1,8 +1,30 @@
-// Chiara Wilson, Functions Notes
+// Chiara Wilson, Functions Notes in C
 #include <stdio.h>
+int num;
+char name[50],place[50],verb[50];
+int add(int numOne,int numTwo){
+return numOne+numTwo;
+
+
+
+}
+const char* word(type){
+    char choice[50];
+    printf(" Please give me a %s:\n",type);
+    scanf("%s",choice);
+    return choice;
+}
+
+
 
 
 int main(void){
-    printf("hello world");
+ //printf("Please tell me a number\n");
+ //scanf("%d",num);
+ //add(num,10);
+ //add(8,1);
+ //add(72,5);
+ //printf("%d",add(72,5));
+
     return 0;
 } 
--- functions/notes.py
+++ functions/notes.py
@@ -1,1 +1,24 @@
 # Chiara Negron Wilson, Function notes
+
+# functions are places that hold actions to be reused
+#number= int(input("Please tell me a number:\n"))
+def add(numOne,numTwo):# Parameters set the name of the variables( Just for trhe function)
+    return numOne+numTwo
+
+    #print (numOne+numTwo)
+
+
+#print(add(number+21))# arguments set the value of variable.
+#print(add(2,100))
+#print(add(3,50))
+
+def values(type):
+    return input(f"Please give me a {type}:\n")
+
+name =values("name")
+
+place =values("place")
+
+verb =values("verb(past tense)")
+
+print(f"{name} was really fast getting to {place} because they {verb} the whole way there.")
