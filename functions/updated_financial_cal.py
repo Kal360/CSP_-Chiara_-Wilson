98f298eb2fe2ca50302413a32254fa82d5812b5e
kal360 <chiara.negron@ucas-edu.net>
2025-02-19 11:49:12 -0700

    I made files for updated financial calc in c and python and I code in them

---
+++ functions/updated_financial_cal.py
@@ -0,0 +1,39 @@
+# Chiara Wilson, Updated Finacial calculator python
+
+# print statement that welcomes my user and tells them what the program does
+
+# ask user what their income is ( variable an input)
+income= float(input("What is your income?\n"))
+# ask user what their rent is ( variable an input)
+rent=float(input("What is your rent?\n"))
+#ask user what their utilites is ( variable an input)
+utilites=float(input("How much do you spend on utilites?\n"))
+#ask user what their groceries is ( variable an input)
+groceries=float(input("How do you spend on groceries?\n"))
+#ask user what their transportation is ( variable an input)
+transportation=float(input(" How much do you spend transportation\n"))
+# calculate savings as 10% of income (income * .1)( variable)
+savings=income*0.1
+# calculate spending as income- savings-rent-utilites-groceries-transportaion ( variable)
+spending=income-savings-rent-utilites-groceries-transportation-savings
+# calculate percent income of rent(rent/income *100) ( variable)
+rentpercent= (rent/income)*100
+# calculate percent income of utilites (utilites/income *100) ( variable)
+utilpercent=(utilites/income)*100
+# calculate percent income of groceries (groceries/income *100) ( variable)
+groceriespercent=(groceries/income)*100
+# calculate percent income of transportation(transportation/income *100) ( variable)
+transportpercent=(transportation/income)*100
+# calculate percent income of spending(spending/income *100) ( variable)
+spendpercent=(spending/income)*100
+
+# Your utilites is $XX.XX which is XX% of your income. (Print)
+print("Your utilites is",utilites, "which is %", utilpercent, "of your income. \n")
+# Your groceries is $XX.XX which is XX% of your income. (Print)
+print("Your groceries is", groceries,"Which is ",groceriespercent, "of your income.\n")
+# Your transportation is $XX.XX which is XX% of your income. (Print)
+print("Your transportation is ", transportation," which is % ", transportpercent, " of your income.\n")
+# Your savings is $XX.XX which is XX% of your income. (print)
+print("Your savings are",savings,"which is 10%% of your income.\n")
+# Your spending is $XX.XX which is XX% of your income. (Print)
+print("Your spending is",spending,"Which is %",spendpercent,"of your income.\n")
---
+++ functions/updated_financial_calculator.c
@@ -0,0 +1,97 @@
+// Chiara Wilson,Updated Finnacial Calculator
+#include <stdio.h>
+#include <math.h>
+
+//print statement that welcomes my user and tells them what the program does
+
+//ask user what their income is ( variable an input)
+float income,rent,utilites,groceries,transportation,savings,expenses,total;
+
+
+float prent,putilites,pgroceries,ptransportation,psavings, pexpenses;
+
+//ask user what their rent is ( variable an input)
+
+//#ask user what their utilites is ( variable an input)
+
+//#ask user what their groceries is ( variable an input)
+
+//ask user what their transportation is ( variable an input)
+
+ //calculate savings as 10% of income (income * .1)( variable)
+
+//calculate spending as income- savings-rent-utilites-groceries-transportaion ( variable)
+
+//calculate percent income of rent(rent/income *100) ( variable)
+
+//calculate percent income of utilites (utilites/income *100) ( variable)
+
+//calculate percent income of groceries (groceries/income *100) ( variable)
+
+//calculate percent income of transportation(transportation/income *100) ( variable)
+
+//calculate percent income of spending(spending/income *100) ( variable)
+
+
+// Your utilites is $XX.XX which is XX% of your income. (Print)
+
+//Your groceries is $XX.XX which is XX% of your income. (Print)
+
+//Your transportation is $XX.XX which is XX% of your income. (Print)
+
+// Your savings is $XX.XX which is XX% of your income. (print)
+
+// Your spending is $XX.XX which is XX% of your income. (Print)
+int main(void){
+    printf("Hi, welcome to my financial calculator\n");
+
+printf(" How much money do you make each month?\n");
+
+scanf("%f",&income);
+
+printf("How much do you pay for rent?\n");
+
+scanf("%f",&rent);
+
+printf("How much do you pay for utilites?\n");
+
+scanf("%f",&utilites);
+
+printf("How mcuh do you pay for groceries?\n");
+
+scanf("%f",&groceries);
+
+printf("How much do you pay for transportation?\n");
+
+scanf("%f",&transportation);
+
+expenses= rent+utilites+groceries+transportation;
+
+savings= income *.2;
+
+total= income-savings-expenses;
+
+printf(" Your monthly income is $%.2f\n",income);
+
+
+printf("Your monthly expenses are $%.2f\n",expenses);
+
+printf("Your monthly savings are $%.2f\n",savings);
+
+printf("You have $%.2f\n",total );
+
+prent= rent/income*100;
+
+putilites= utilites/income*100;
+
+pgroceries= groceries/income*100;
+
+ptransportation= transportation/income*100;
+
+pexpenses=expenses/income*100;
+
+psavings=savings/income*100;
+
+printf("Your expenses are %.1f%% of your income.\n",pexpenses);
+    return 0;
+} 
