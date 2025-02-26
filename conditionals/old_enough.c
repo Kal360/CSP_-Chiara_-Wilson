bb1e1b72fe9ef7937f7628327aa25d9b0de54ec3
kal360 <chiara.negron@ucas-edu.net>
2025-02-26 11:49:35 -0700

    I coded in c and python to fix stuff

--- conditionals/old_enough.c
+++ conditionals/old_enough.c
@@ -6,9 +6,9 @@ int main(void){
     printf("How old are you?\n");
     scanf("%d,",&num);
     if(num>=18){
-        printf("%d is old enough to vote!\n",num);
-    }else if (num <18){
-        printf(" %d is not old enough to vote.\n",num);
+        printf("%d is old enough to vote, go to school!\n",num);
+    }else if (num <18==16){
+        printf(" %d is not old enough to vote but is old enough to drive, get a permit and go to school\n",num);
     
 } 
 
---
+++ conditionals/time_of_day.c
@@ -0,0 +1,9 @@
+// Chiara Wilson, TEMPLATE
+#include <stdio.h>
+int num;
+
+int main(void){
+    printf(" What time of day is it?\n");
+    scanf("%d,",&num);
+    return 0;
+} 
