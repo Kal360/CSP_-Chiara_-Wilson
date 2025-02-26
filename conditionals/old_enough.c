393317cf9cf54bd601fdce374974bf74a23ce834
kal360 <chiara.negron@ucas-edu.net>
2025-02-26 11:38:10 -0700

    I coded my old enough practice in c and python

--- conditionals/old_enough.c
+++ conditionals/old_enough.c
@@ -3,6 +3,35 @@
 int num;
 
 int main(void){
-    if(num)
-    return 0;
+    printf("How old are you?\n");
+    scanf("%d,",&num);
+    if(num>=18){
+        printf("%d is old enough to vote!\n",num);
+    }else if (num <18){
+        printf(" %d is not old enough to vote.\n",num);
+    
 } 
+
+if(num>=16){
+    printf(" %d is old enough to drive\n",num);
+}else if (num<16){
+    printf(" %d is not old enough to drive\n",num);
+}
+if( num>=15){
+    printf(" %d is old enough to get your learners permit!\n",num);
+}else if (num<15){
+    printf("You are not old enough to get a learners permit.\n ",num);
+}
+
+if (num>=3){
+    printf(" %d is old enough to go to school!\n",num);
+}else if (num<3){
+    printf(" %d is not old enough to go to school.\n",num);
+}
+
+return 0;
+
+
+
+
+}
---
+++ conditionals/old_enough.py
@@ -0,0 +1,21 @@
+# Chiara Ivelisse Negron Wilson, Old enough python practice
+num=int(input(" How old are you ? \n"))
+if num >=18:
+    print(" You are old enough to vote! \n")
+elif num<18:
+    print(" You are not old enough to vote. \n")
+
+if num >= 16:
+    print(" You are old enough to drive! \n")
+elif num <16:
+    print(" You are not old enough to drive. \n")
+
+if num >=15:
+    print(" You are old enough to get a learner's permit ! \n")
+elif num <16:
+    print(" You are not old enough to get a learner's permit.\n")
+
+if num >=3 :
+    print(" You are old enough to go to school! \n")
+elif num <3:
+    print(" You are not old enough to go to school. \n")
