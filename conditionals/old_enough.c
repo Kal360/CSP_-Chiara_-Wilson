e722e28e1b1f6ebb3ec1c30c3138813f6d8467f7
Kal360 <chiara.negron@ucas-edu.net>
2025-02-27 13:40:47 +0000

    I coded more in C and added an else statement

--- conditionals/old_enough.c
+++ conditionals/old_enough.c
@@ -11,11 +11,15 @@ int main(void){
         printf(" %d is not old enough to vote but is old enough to drive, get a permit and go to school\n",num);
     }else if (num <18== 15){
         printf("%d is old enough to go to school and get a permit but not old enough to drive, or vote.\n",num);
-    } else if (num <3)
+    } else if (num <3){
     printf(" %d is not old enough to go to school, get a permit, drive, or vote.\n",num);
-    return 0;
-} 
+    }else{
+        printf(" You are very old.\n");
 
+
+    return 0;
+}
+}
 
 
 
