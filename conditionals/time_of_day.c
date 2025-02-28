9169a7282f42ef6e5087d319b060a9bfc9f53dc6
kal360 <chiara.negron@ucas-edu.net>
2025-02-28 11:23:23 -0700

    I coded my time of day project in C and python

--- conditionals/time_of_day.c
+++ conditionals/time_of_day.c
@@ -1,9 +1,25 @@
 // Chiara Wilson, TEMPLATE
 #include <stdio.h>
+#include <time.h>
 int num;
 
 int main(void){
-    printf(" What time of day is it?\n");
-    scanf("%d,",&num);
-    return 0;
+time_t rawtime;
+struct tm * timeinfo;
+
+   time_t now= time(NULL);
+
+struct tm* tm_struct = localtime(&now);
+int hour= tm_struct->tm_hour;
+
+if(hour>=0 && hour<=11){
+    printf("Good morning!\n");
+}else if( hour== 12 && hour <=16){
+    printf("Good afternoon!\n");
+}else if ( hour ==17 && hour <= 20){
+    printf("Good evening! \n");
+}else{
+    printf("Good night\n");
+}
+    return 0; 
 } 
--- conditionals/time_of_day.py
+++ conditionals/time_of_day.py
@@ -4,8 +4,16 @@ current=time.time()
 local_time=time.localtime(current)
 day = local_time.tm_wday
 hour = local_time.tm_hour
-if hour== 0-11 :
+
+if hour >= 0 and hour <= 11:
     print("Good Morning!\n")
+elif hour == 12 and hour <= 16:
+    print("Good afternoon!\n")
+elif hour==17 and hour <=20:
+    print("Good evening\n")
+else:
+    print("Good night\n")
+
 
 
 
