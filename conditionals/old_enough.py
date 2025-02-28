2fcb186bd0cdde01c7cd9116f01112ac7acffe17
Kal360 <chiara.negron@ucas-edu.net>
2025-02-28 16:46:02 +0000

    I coded and fixed my old enough in python

--- conditionals/old_enough.py
+++ conditionals/old_enough.py
@@ -2,22 +2,16 @@
 num=int(input(" How old are you ? \n"))
 if num >=18:
     print(" You are old enough to vote,drive,get a permit and to go to school! \n")
-elif num<18==16:
-    print(" You are old enough to drive,go to school but not to vote. \n")
+elif num<18 and num >=16:
+    print(" You are old enough to drive,go to school and get a permit but not to vote. \n")
 
-elif num >= 16:
-    print(" You are old enough to drive,go to school but not to vote. \n")
-elif num <16:
-    print(" You are not old enough to drive. \n")
+elif num ==15:
+    print(" You are old enough to go to school and get a permit, but not to vote or drive. \n")
 
-elif num >=15:
-    print(" You are old enough to get a learner's permit ! \n")
-elif num <16:
-    print(" You are not old enough to get a learner's permit.\n")
+elif num < 3:
+    print(" You are very young.\n")
+elif num <15 or num ==3:
+    print(" You are old enough to go to school but not to get a permit,drive, or vote.\n")
 
-elif num >=3 :
-    print(" You are old enough to go to school! \n")
-elif num <3:
-    print(" You are not old enough to go to school,vote or drive. \n")
 else: 
     print(" You are either very young or something went wrong in my code.\n")
