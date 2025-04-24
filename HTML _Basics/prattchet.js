06228b8b75fabee7ebb524e5712b4c7522e0297e
Kal360 <chiara.negron@ucas-edu.net>
2025-04-24 23:15:06 +0000

    I tired fixing my code to no avail in first website.

--- HTML _Basics/prattchet.js
+++ HTML _Basics/prattchet.js
@@ -1,9 +1,19 @@
 function view(){
-    if(document.getElementById("more").style.display==="block"){ document.getElementById ("more").style. display = "none"
-        document.getElementById ("more").style. display = "block"
-         document.getElementById ("shw").innerHTML = "Show Less"
+    if(document.getElementById("hidden image").style.display == "block"){
+      document.getElementById("hidden image").style.display = "none"
+      document.getElementById("shw").innerHTML = "Show More"
     }else{
-        document.getElementById ("more").style. display = "block"
-        document.getElementById ("shw").innerHTML = "Show Less"
+      document.getElementById("hidden image").style.display = "block"
+      document.getElementById("shw").innerHTML = "Show Less"
     }
+}
+function change(){
+    if (counter < images.length){
+       document.getElementById("img").src = images[counter] 
+       counter += 1
+    }else{
+        counter = 0
+        document.getElementById("img").src = images[counter]
+    }
+
 }
