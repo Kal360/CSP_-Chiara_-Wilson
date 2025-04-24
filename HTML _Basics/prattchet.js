c2951368dab5e6a29e135e7e4a5368327621368a
Kal360 <chiara.negron@ucas-edu.net>
2025-04-24 23:26:34 +0000

    sss

--- HTML _Basics/prattchet.js
+++ HTML _Basics/prattchet.js
@@ -1,9 +1,9 @@
 function view(){
-    if(document.getElementById("Hidden Image").style.display == "block"){
-      document.getElementById("Hidden image").style.display = "none"
+    if(document.getElementById("more").style.display == "block"){
+      document.getElementById("more").style.display = "none"
       document.getElementById("shw").innerHTML = "Show More"
     }else{
-      document.getElementById("Hidden image").style.display = "block"
+      document.getElementById("more").style.display = "block"
       document.getElementById("shw").innerHTML = "Show Less"
     }
 }
