87c00cbf1c5ba9ecfe9428fed327c8e039df55b3
Kal360 <chiara.negron@ucas-edu.net>
2025-04-24 11:48:34 -0600

    I messed around and triued to update my website

---
+++ HTML _Basics/prattchet.js
@@ -0,0 +1,9 @@
+function view(){
+    if(document.getElementById("more").style.display==="block"){ document.getElementById ("more").style. display = "none"
+        document.getElementById ("more").style. display = "block"
+         document.getElementById ("shw").innerHTML = "Show Less"
+    }else{
+        document.getElementById ("more").style. display = "block"
+        document.getElementById ("shw").innerHTML = "Show Less"
+    }
+}
--- javascript/javascript_notes.js
+++ javascript/javascript_notes.js
@@ -34,5 +34,16 @@ let current = document.getElementById("meme").style.display
      function pop(){
         window.alert("Really don't click that!")
      }
+     function view(){
+        if(document.getElementById("more").style.display==="block"){ document.getElementById ("more").style. display = "none"
+            document.getElementById ("more").style. display = "block"
+             document.getElementById ("shw").innerHTML = "Show Less"
+        }else{
+            document.getElementById ("more").style. display = "block"
+            document.getElementById ("shw").innerHTML = "Show Less"
+        }
+
+        
+     }
      
  
