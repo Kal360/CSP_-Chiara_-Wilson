3f678a6042c13626f76b3dd8478d6006650fadfb
Kal360 <chiara.negron@ucas-edu.net>
2025-04-25 00:56:27 +0000

    I tired to do the hover change image thing on my website. It worked.

--- HTML _Basics/prattchet.js
+++ HTML _Basics/prattchet.js
@@ -7,13 +7,15 @@ function view(){
       document.getElementById("shw").innerHTML = "Show Less"
     }
 }
+let images=["https://m.media-amazon.com/images/I/51SQtkuDJNL._AC_UF1000,1000_QL80_.jpg","https://upload.wikimedia.org/wikipedia/en/c/c2/Susan_sto_helit_promo.jpg","https://tabularasabookblog.wordpress.com/wp-content/uploads/2020/11/807a2-discworlddeath.jpg"]
+let counter=0
 function change(){
     if (counter < images.length){
-       document.getElementById("img").src = images[counter] 
+       document.getElementById("stuff").src = images[counter] 
        counter += 1
     }else{
         counter = 0
-        document.getElementById("img").src = images[counter]
+        document.getElementById("stuff").src = images[counter]
     }
 
 }
