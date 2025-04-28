8d06379fa332134931577206ca57c1b7208e10be
Kal360 <chiara.negron@ucas-edu.net>
2025-04-28 16:47:03 +0000

    I fixed the hover image thing on my website and I centerd the button.

--- Internet_Final/stay.js
+++ Internet_Final/stay.js
@@ -4,11 +4,11 @@ function view(){
       document.getElementById("shw").innerHTML = "Show More"
     }else{
       document.getElementById("more").style.display = "block"
-      document.getElementById("shw").innerHTML = "Show Less"}
+      document.getElementById("shw").innerHTML = "Show Less"
+    }
     }
      
-    let image= ["https://cf.bstatic.com/xdata/images/hotel/max1024x768/206317777.jpg?k=d8c98395334a3fcaf797dc7c552a215da5f6535c1a5457098f3498e5d222c169&o=&hp=1","https://teneohg.com/wp-content/uploads/2024/09/Sundance-Mountain-Resort-Bar-845x667.webp","https://www.sundanceresort.com/wp-content/uploads/2023/04/Matt-Morgan-Tree-Room-1-scaled.webp"]
-
+    let images= ["https://cf.bstatic.com/xdata/images/hotel/max1024x768/206317777.jpg?k=d8c98395334a3fcaf797dc7c552a215da5f6535c1a5457098f3498e5d222c169&o=&hp=1","https://teneohg.com/wp-content/uploads/2024/09/Sundance-Mountain-Resort-Bar-845x667.webp","https://www.sundanceresort.com/wp-content/uploads/2023/04/Matt-Morgan-Tree-Room-1-scaled.webp"]
     let counter=0
 function change(){
     if (counter < images.length){
